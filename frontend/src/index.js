261cdae5be515be1106cbafdd6e1c2e53fba60f0
Jan Tuomi <jan.tuomi@eficode.com>
2018-05-13 19:04:52 +0300

    Ensure that backend URL is set

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 //import registerServiceWorker from './registerServiceWorker';
 
+if (!process.env.REACT_APP_BACKEND_URL) {
+  throw new Error('No backend URL set. Please set REACT_APP_BACKEND_URL in .env!');
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 //registerServiceWorker();
