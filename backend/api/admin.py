e7e67c926951dc0bf88e784f00cf0b3d2103f7e6
Jan Tuomi <jan.tuomi@eficode.com>
2018-09-02 18:30:43 +0300

    Add action to mark all transactions as done for profile

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -18,6 +18,14 @@ def set_not_done(modeladmin, request, queryset):
 
 set_not_done.short_description = "Mark selected transactions as not done"
 
+def set_profile_all_done(modeladmin, request, queryset):
+  """Action to set all transactions as True."""
+  #pylint: disable=W0613
+  for user in queryset:
+    transactions = Transaction.objects.filter(user=user).update(done=True)
+
+set_profile_all_done.short_description = "Mark all transactions as done for profile"
+
 class TransactionAdmin(admin.ModelAdmin):
   """Transaction admin."""
   list_display = ('name', 'user', 'price_in_euros', 'timestamp', 'done')
@@ -26,6 +34,7 @@ class TransactionAdmin(admin.ModelAdmin):
 class UserProfileAdmin(admin.ModelAdmin):
   """User Profile admin."""
   list_display = ('user', 'balance', 'visible')
+  actions = [set_profile_all_done]
 
 class ProductAdmin(admin.ModelAdmin):
   """Product admin."""
