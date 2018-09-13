ca6e1c3f933c991573e29d50d84bf2cd2f61f669
Panu Oksiala <panu@oksiala.fi>
2018-09-14 00:05:47 +0300

    Show requester first in userprofiles listing.
    also fix goofy intendation

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,6 +3,7 @@ Sähköpiikki views.
 """
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from api.models import UserProfile, Transaction, Product
 from api.serializers import (
@@ -11,21 +12,37 @@ from api.serializers import (
 )
 
 class UserViewset(viewsets.ReadOnlyModelViewSet):
-  """User viewset."""
-  queryset = User.objects.all()
-  serializer_class = UserSerializer
+    """User viewset."""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class UserProfileViewSet(viewsets.ReadOnlyModelViewSet):
-  """User Profile viewset."""
-  queryset = UserProfile.objects.filter(visible=True).order_by('user__first_name')
-  serializer_class = UserProfileSerializer
+    """User Profile viewset."""
+    queryset = UserProfile.objects.filter(visible=True).order_by('user__first_name')
+    serializer_class = UserProfileSerializer
+
+    def get_queryset(self):
+        """ Get Queryset
+        If requester is authenticated and is included in the
+        queryset he/she will be returned as a first result
+
+        Small increase in database load but should not matter in
+        this context
+        """
+        user = self.request.user
+        if not user.is_anonymous:
+            requester = self.queryset.filter(user=user)
+            rest = self.queryset.exclude(user=user)
+            return list(requester) + list(rest)
+        return self.queryset
 
 class ProductViewSet(viewsets.ReadOnlyModelViewSet):
-  """Product viewset."""
-  queryset = Product.objects.filter(visible=True)
-  serializer_class = ProductSerializer
+    """Product viewset."""
+    queryset = Product.objects.filter(visible=True)
+    serializer_class = ProductSerializer
 
 class TransactionViewSet(viewsets.ModelViewSet):
-  """Transaction viewset."""
-  queryset = Transaction.objects.all()
-  serializer_class = TransactionSerializer
+    """Transaction viewset."""
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
