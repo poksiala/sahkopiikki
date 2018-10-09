176f681377024f7b0f1c3a810d67f8304f393ff0
Jan Tuomi <jan.tuomi@eficode.com>
2018-10-09 23:40:04 +0300

    Add bind host to gunicorn command

--- deploy/start-production-backend.sh
+++ deploy/start-production-backend.sh
@@ -10,4 +10,4 @@ cd "${DIR}/../backend"
 . ~/venv/bin/activate
 
 # Start Django with Gunicorn
-gunicorn -w 4 --access-logfile - --error-logfile - sahkopiikki.wsgi
+gunicorn --bind 127.0.0.1:8000 -w 4 --access-logfile - --error-logfile - sahkopiikki.wsgi
